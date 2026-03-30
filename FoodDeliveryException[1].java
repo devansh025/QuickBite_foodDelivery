e08c7b0f94e3cbb6ab12921e0f0368a5afa9a7c3
Devansh Rathore <devanshrathore025@gmail.com>
2026-03-30 17:16:04 +0530

    Delete FoodDeliveryException[1].java

--- FoodDeliveryException[1].java
+++
@@ -1,8 +0,0 @@
-// custom exception i made for handling errors in this app
-// instead of using generic Exception everywhere
-public class FoodDeliveryException extends Exception {
-
-    public FoodDeliveryException(String message) {
-        super(message);
-    }
-}
